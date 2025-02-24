7bded928edb168d31498e947d6ec9458e81488a5
Lorenzo Giambellini <lorenzo.giambellini@icloud.com>
2025-02-24 16:22:50 +0100

    add playRound function

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-console.log("hello");
-
 function getComputerChoice() {
     let x = Math.random()*100;
-    console.log(x);
     if (x < 33) return "rock";
     else if (x > 66) return "paper";
     else return "scissors";
@@ -15,5 +12,27 @@ function getHumanChoice() {
     else if (x == "P") return "paper";
     else if (x == "S") return "scissors";
 }
-console.log(getHumanChoice());
 
+
+let humanScore = 0; 
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === "rock"){
+        if(computerChoice === "paper") computerScore +=1;
+        if(computerChoice === "scissors") humanScore +=1;
+    }
+    if(humanChoice === "paper"){
+        if(computerChoice === "rock") humanScore +=1;
+        if(computerChoice === "scissors") computerScore +=1;
+    }
+    if(humanChoice === "scissors"){
+        if(computerChoice === "rock") computerScore +=1;
+        if(computerChoice === "paper") humanScore +=1;
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);
