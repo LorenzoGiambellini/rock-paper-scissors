88f2fc2b6753aaa3e653226da3a92b7ea47c94cb
Lorenzo Giambellini <lorenzo.giambellini@icloud.com>
2025-02-24 17:31:19 +0100

    add 5 rounds to the game

--- script.js
+++ script.js
@@ -3,36 +3,47 @@ function getComputerChoice() {
     if (x < 33) return "rock";
     else if (x > 66) return "paper";
     else return "scissors";
-}
-console.log(getComputerChoice());
+    }
 
 function getHumanChoice() {
-    let x = prompt("choose your weapon:\nR: rock\nP: paper\nS: scissors");
-    if (x == "R") return "rock";
-    else if (x == "P") return "paper";
-    else if (x == "S") return "scissors";
+    let x = prompt("choose your weapon:\n- rock\n- paper\n- scissors");
+
+    return x.toLowerCase();
 }
 
 
-let humanScore = 0; 
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice === "rock"){
-        if(computerChoice === "paper") computerScore +=1;
-        if(computerChoice === "scissors") humanScore +=1;
+
+function playGame(){
+    let humanScore = 0; 
+    let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
+    console.log(computerSelection);
+    
+    function playRound(humanChoice, computerChoice) {
+        if(humanChoice === "rock"){
+            if(computerChoice === "paper") computerScore +=1;
+            if(computerChoice === "scissors") humanScore +=1;
+        }
+        if(humanChoice === "paper"){
+            if(computerChoice === "rock") humanScore +=1;
+            if(computerChoice === "scissors") computerScore +=1;
+        }
+        if(humanChoice === "scissors"){
+            if(computerChoice === "rock") computerScore +=1;
+            if(computerChoice === "paper") humanScore +=1;
+        }
     }
-    if(humanChoice === "paper"){
-        if(computerChoice === "rock") humanScore +=1;
-        if(computerChoice === "scissors") computerScore +=1;
-    }
-    if(humanChoice === "scissors"){
-        if(computerChoice === "rock") computerScore +=1;
-        if(computerChoice === "paper") humanScore +=1;
+    for(let i = 0; i < 5; i++){
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log("Human Choice: " + humanSelection);
+        console.log("Computer Choice: " + computerSelection);
+        console.log("Human Score: " + humanScore);
+        console.log("Computer Score: " + computerScore);
     }
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
-console.log("Human Score: " + humanScore);
-console.log("Computer Score: " + computerScore);
+
+playGame();
